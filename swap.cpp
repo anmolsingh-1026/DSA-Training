fe1d670d712998e64cfa0de45241325e97b29f19
Anmol Singh <anmolsingh-1026@gmail.com>
2026-02-11 10:21:36 +0530

    Your descriptive commit message

---
+++ swap.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Function to reverse the array
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end) {
+        // Swap elements
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = 5;
+
+    reverseArray(arr, size);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
