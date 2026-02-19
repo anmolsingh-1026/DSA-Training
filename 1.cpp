5a67a6158165f51a36a0585e67c10537120e4286
Anmol Singh <anmolsingh-1026@gmail.com>
2026-02-19 09:57:20 +0530

    prefixsum

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
-using namespace std;    
-int main()
-{
-    cout<<"Hello World";
+using namespace std;
+
+int main(){
+     
+    int arr[] = {2,4,6,8,10};
+    int size = 5;
+
+    int prefixSum[size];
+    prefixSum[0] = arr[0];
+   
+   
+    for(int i=1; i<size; i++){
+        prefixSum[i] = prefixSum[i-1] + arr[i];
+    }
+    
+    for(int i=0; i<size; i++){
+        cout<<prefixSum[i]<<" ";
+    }
     return 0;
 }
 
